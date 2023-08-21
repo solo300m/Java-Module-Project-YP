cf2616dc76dc3795bbb8504e4735fe3b8bd057c6
Sergey Lyulchenko <solo300m@yandex.ru>
2023-08-21 15:58:15 +0400

    commit HW

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+    private List<Product>list = new ArrayList<Product>();
+    private int person = 0;
+    Calculator(int person){
+        this.person = person;
+    }
+
+    public List<Product> getList() {
+        return list;
+    }
+    public void add(Product product){
+        list.add(product);
+    }
+    public double sum(){
+        double sum = 0.0;
+        for(Product p : list){
+            sum += p.getPrice();
+        }
+        return ((double)Math.round(sum * 100)) / 100;
+    }
+    public double calcPastPay(){
+        if(this.person > 0)
+            return ((double)Math.round(sum() / this.person * 100)) / 100;
+        else
+            return 0.0;
+    }
+    public void printCheck(){
+        for(Product p: getList()){
+            System.out.println(p.toString());
+        }
+    }
+    public String rubRightWrite(double sum){
+        int dec = (((int) sum) / 10) % 10;
+        if(dec == 1){
+            return "рублей";
+        }else {
+            int delta = ((int) sum) % 10;
+            switch (delta) {
+                case 1:
+                    return "рубль";
+                case 2:
+                case 3:
+                case 4:
+                    return "рубля";
+                case 0:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                    return "рублей";
+            }
+        }
+        return "руб.";
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,73 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("На сколько персон необходимо поделить счет: ");
+        Scanner scanner = new Scanner(System.in);
+        int countPerson = 0;
+        // String nameProduct = "";
+        double price = 0.0;
+        String next = "";
+        Calculator calc = null;
+
+        countPerson = scanner.nextInt();
+        if (countPerson <= 0){
+            System.out.println("Количество человек не может быть отрицательным или равным нулю!");
+        }else if(countPerson == 1){
+            System.out.println("Нет смысла в работе программы при количестве персон равном 0");
+        }else {
+            System.out.println("Вы планируете разделить счета на "+countPerson+" чел.");
+            calc = new Calculator(countPerson);
+            while(!next.equals("завершить")){
+                System.out.println("Введите наименование товара: ");
+                System.out.print("-> ");
+                String tmp = scanner.next();
+
+                boolean check = false;
+                String strPrice = "";
+                while (!check) {
+                    System.out.print("-> ");
+                    strPrice = scanner.next();
+                    check = verifyDouble(strPrice);
+                    if(!check){
+                        System.out.println("Не правильный ввод. Поробуйте еще.");
+                    }
+                }
+                price = Double.parseDouble(strPrice);
+                Product product = new Product(tmp,price);
+                calc.add(new Product(tmp,price));
+                System.out.println("Добавить еще товар? Да: любой символ + Enter, Закончить ввод: Завершить + Enter");
+                next = (scanner.next().toLowerCase());
+            }
+            System.out.println("Итоговый чек: ");
+            calc.printCheck();
+            System.out.println("К оплате итого: "+calc.sum()+" "+calc.rubRightWrite(calc.sum()));
+            System.out.println("В том числе с каждого члена команды "+calc.calcPastPay()+" "+calc.rubRightWrite(calc.calcPastPay()));
+        }
+
     }
+    public static boolean verifyDouble(String str){
+        boolean rez = false;
+        char[] chArr = str.toCharArray();
+        int countPoint = 0;
+        for(char i: chArr){
+            if(Character.isDigit(i)){
+                rez = true;
+            }else if(i == '.'){
+                countPoint++;
+            }else if(!Character.isDigit(i) && i != '.'){
+                rez = false;
+                return rez;
+            }
+        }
+        if(countPoint <= 1){
+            return rez;
+        }else{
+            rez = false;
+            return rez;
+        }
+    }
+
 }
